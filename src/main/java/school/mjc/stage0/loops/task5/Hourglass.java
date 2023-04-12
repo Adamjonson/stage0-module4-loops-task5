1746795dbd0db0dda04d221095516c3b9b96bab6
Hp <adkhamjon.abd@gmail.com>
2023-04-12 15:04:36 +0200

    Corrects some mistakes

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -29,7 +29,7 @@ public class Hourglass {
     }
     public void printHourglassOfGivenSize(int height) {
         if (height == 0){
-            System.out.println("");
+            System.out.print("");
         }else {
             int space = 1;
             hours(height, space);
